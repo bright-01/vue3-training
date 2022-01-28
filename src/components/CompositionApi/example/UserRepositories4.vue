f5e15ce74a6d49f5246f22f9242decfdb9243070
쿠도커뮤니케이션 천명원 <cjsaud90@naver.com>
2022-01-28 14:42:44 +0900

    composition 예제

---
+++ src/components/CompositionApi/example/UserRepositories4.vue
@@ -0,0 +1,77 @@
+<template>
+
+</template>
+
+
+<script>
+import {fetchUserRepositories} from "@/api/TestApi";
+import {ref, toRefs, onMounted, watch, computed} from 'vue';
+
+export default {
+  name: "UserRepositories1",
+  props: {
+    type: String
+  },
+  setup(props) {
+    console.log('setup!');
+    const {user} = toRefs(props);
+    // let repositories = []; 아래와 같은 방식으로 선언해서 반응형 참조를 만든다.
+    let repositories = ref([]);
+    const getUserRepositories = async () => {
+      // props.user의 참조 .value에 접근하기 위해서 'user.value'로 변경
+      repositories = await fetchUserRepositories(user.value);
+    }
+
+    //라이프사이클 훅 추가
+    onMounted(getUserRepositories);
+
+    // props로 받고 반응성참조가 된 user에 감시자를 세팅
+    watch(user, getUserRepositories)
+
+    const searchQuery = ref('');
+    const repositoriesMatchingSearchQuery = computed(() => {
+      repositories.value.filters(
+          repositories => repositories.name.includes(searchQuery.value)
+      )
+    })
+
+    return {
+      repositories,
+      getUserRepositories,
+      searchQuery,
+      repositoriesMatchingSearchQuery
+    }
+  },
+  data() {
+    return {
+      // repositories :[],
+      // filters: {},
+      // searchQuery: ''
+    }
+  },
+  computed: {
+    // filteredRepositories() {
+    // },
+    // repositoriesMatchingSearchQuery() {
+    // }
+  },
+  watch: {
+    // user: 'getUserRepositories'
+  },
+  methods: {
+    // getUserRepositories(){
+    // },
+    // updateFilters() {
+    // }
+  },
+  // mounted(){
+  //   this.getUserRepositories();
+}
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
